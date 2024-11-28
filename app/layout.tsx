2c9e08dbffaaa3202855fa0770d6a1055b38f89b
Sam Huckaby <samhuckaby@gmail.com>
2024-11-27 22:16:28 -0700

    deploy please

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+{/* Need to create an OG image still */}
 export const metadata: Metadata = {
   title: "KM Doula - Here for you",
   description: "KM Doula is a doula service located in Grand Rapids, Michigan that provides birth, lactation, and postpartum support.",
